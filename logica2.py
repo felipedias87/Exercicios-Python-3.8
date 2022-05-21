f3f01d2635407e91103f4a576a5dbedffd61b7b9
felipedias87 <105897491+felipedias87@users.noreply.github.com>
2022-05-21 11:08:02 -0300

    Execicios de lógica Python

---
+++ logica2.py
@@ -0,0 +1,22 @@
+contador = int(input("Digite o numero"))
+array = []
+while contador > 0:
+    contador = contador - 1
+    array.append(contador)
+teste = ' '
+lista_unica = list(set(array))
+for lista in lista_unica:
+    if lista >= 0:
+        cont = 0
+        for a in range(lista):
+            cont = cont + 1
+            teste = teste +" "+ str((a+1))
+            if(cont  == lista):
+                print(teste)
+                teste=' '
+
+
+
+
+
+
